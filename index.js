c265bd7cc995bfc69a4c6ca9428cb83343a475c3
Kevin <kevinolongo9@gmail.com>
2023-09-14 19:26:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const mainElement = document.getElementById('main');
+if (mainElement) {
+  mainElement.remove();
+}
+const newHeader = document.createElement('h1');
+newHeader.textContent = 'New header';
+newHeader.id = 'victory';
+document.body.appendChild(newHeader);
+
+const yourName = "Kevin Olongo"; 
+newHeader.textContent = `${yourName} is the champion`;
